0d210cc643830ef01da68b8fe67ef2aa39445ee6
mrrishi <mrrishi373@gmail.com>
2026-04-01 11:02:45 +0530

    fix: remove driver and fm version

--- chroot.sh
+++ chroot.sh
@@ -420,6 +420,9 @@ apt-get install -y \
 # Configure containerd for Kubernetes (K3s uses containerd)
 nvidia-ctk runtime configure --runtime=containerd
 
+sed -i 's|#root = "/run/nvidia/driver"|root = "/run/nvidia/driver"|' \
+    /etc/nvidia-container-runtime/config.toml
+    
 # echo "=== Installing NVIDIA Driver 570.158.01 ==="
 # NVIDIA_DRIVER_VERSION="570.158.01"
 
